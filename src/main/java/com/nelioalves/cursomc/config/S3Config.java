1909d4627d7becae5f1d86a44507bf4c524965bc
Marcos Lorencini <mlorencinif@gmail.com>
2018-12-07 15:42:02 -0200

    salvando primeiro arquivo no S3

---
+++ src/main/java/com/nelioalves/cursomc/config/S3Config.java
@@ -0,0 +1,42 @@
+package com.nelioalves.cursomc.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+
+//arquivo de configuração S3Config com um bean do tipo AmazonS3
+
+@Configuration
+public class S3Config {
+	
+	//pega as credenciais do application.properties 
+	//para acessar o S3 na Amazon
+	
+	@Value("${aws.access_key_id}")
+	private String awsId;
+
+	@Value("${aws.secret_access_key}")
+	private String awsKey;
+
+	@Value("${s3.region}")
+	private String region;
+	
+	//instancia o objeto AmazonS3
+	@Bean
+	public AmazonS3 s3client() {
+		BasicAWSCredentials awsCred = new BasicAWSCredentials(awsId, awsKey);
+		AmazonS3 s3client = AmazonS3ClientBuilder.standard().withRegion(Regions.fromName(region))
+							.withCredentials(new AWSStaticCredentialsProvider(awsCred)).build();
+		return s3client;
+		
+	}
+	
+	
+
+}
